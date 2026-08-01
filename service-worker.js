6055cc9762a8318ab772c197114d957bfd926f05
padelarenareggioemilia <padelarenareggioemilia@gmail.com>
2026-08-01 14:52:12 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='pam-v8-0-3';
+const CACHE='pam-v8-1-7';
 const CORE=['./','./index.html','./manifest.webmanifest','./assets/padel-arena-manager-logo.png'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).catch(()=>{}));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
