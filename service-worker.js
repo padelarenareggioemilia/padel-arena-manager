35c3bb483e4d2c2b9d120e3806727cf4dca5ae9f
padelarenareggioemilia <padelarenareggioemilia@gmail.com>
2026-09-05 16:42:09 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const PAM_SW_VERSION="823855";
+const PAM_SW_VERSION="823856";
 self.addEventListener("install",event=>{self.skipWaiting()});
 self.addEventListener("activate",event=>{
  event.waitUntil((async()=>{
